395e559ed8a171b39c1fb92c791d6e6046defefd
Mohamed Abdulalim <megyptm@gmail.com>
2023-05-10 17:18:29 +0300

    1.0.3

--- uploader/Base64/UploadBase64.php
+++ uploader/Base64/UploadBase64.php
@@ -18,6 +18,10 @@ abstract class UploadBase64 extends Mime2extPDF
     protected string $file_dir;
     protected string $file_name;
 
+    protected int $max_width = 0;
+    protected int $max_height = 0;
+    protected int $max_size = 0;
+
     protected function Upload(): string
     {
         if (empty($_POST['base64_file'])) {
@@ -36,6 +40,16 @@ abstract class UploadBase64 extends Mime2extPDF
             $extension = $this->mime2extPDF($mime_type); // extract extension from mime type
         }
         if (!empty($extension)) {
+            $size = getimagesizefromstring($decoded_file);
+            if(!empty($this->max_width) && $size[0] > $this->max_width){
+                Json::Incorrect('max_width', 'cannot more than ' . $this->max_width);
+            }
+            if(!empty($this->max_height) && $size[1] > $this->max_height){
+                Json::Incorrect('max_height', 'cannot more than ' . $this->max_height);
+            }
+            if(!empty($this->max_size) && /*filesize($decoded_file) */strlen($decoded_file) > $this->max_size*1024){
+                Json::Incorrect('max_size', 'cannot more than ' . $this->max_size . ' MB');
+            }
             if(empty($this->file_name)){
                 $fileName = round(microtime(true) * 1000) . uniqid();
                 $file = $this->uploaded_for_id . '_' . time() . "_" . $fileName . uniqid() . '.' . $extension;
