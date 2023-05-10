b04236ae17357798b9040fafe03ad1a6e8af2e07
Mohamed Abdulalim <megyptm@gmail.com>
2023-05-10 17:18:29 +0300

    1.1.7

--- uploader/Images/UploadImage.php
+++ uploader/Images/UploadImage.php
@@ -13,7 +13,7 @@ use Maatify\Uploader\Mime\Mime2extPDF;
 
 class UploadImage extends Mime2extPDF
 {
-    protected int $uploaded_for_id;
+    protected int|string $uploaded_for_id;
     protected string $upload_folder;
     protected string $file_target;
     protected string $file_name;
